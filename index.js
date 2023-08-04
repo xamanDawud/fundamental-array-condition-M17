1fa0a1e25c7f0f0ee3b74d8406304d60d42d47b5
xamanDawud <ahmeddaudking100@gmail.com>
2023-08-04 13:06:59 +0600

    aray indexing has been  explored

---
+++ index.js
@@ -0,0 +1,12 @@
+var colors = ["red", "blue", "aquamarin", "yellow", "redlight"];
+var age = [23, 53, 26, 66, 63, 77, 23];
+
+// Get value by index Number
+console.log(colors[2]);
+
+// Get index number by provide value
+console.log(age.indexOf(66));
+
+// Reset value by the index
+colors[4] = "Purple";
+console.log(colors);
