57c3d66a3e21c006b1b6767f03bf5fe59836b188
xamanDawud <ahmeddaudking100@gmail.com>
2023-08-05 01:08:12 +0600

    multiple condition added

---
+++ conditions.js
@@ -0,0 +1,10 @@
+var price = 3000;
+var budget = 500;
+
+if (price > budget) {
+  console.log("Mama tumar samorto naih");
+}
+
+if (budget > price) {
+  console.log("Mama ekoni kine fel");
+}
---
+++ multiple-conditions.js
@@ -0,0 +1,19 @@
+var isGraduate = true;
+var salary = 54000;
+var home = false;
+
+if (isGraduate == true && salary >= 50000 && home == true) {
+  console.log("Tumi amake peye gecho");
+} else {
+  console.log("Tumar kopal kahara bodda");
+}
+
+var isGraduate = true;
+var salary = 54000;
+var home = false;
+
+if ((isGraduate === true && home === true) || salary >= 50000) {
+  console.log("Tumakei ami chai");
+} else {
+  console.log("Try to other side baby");
+}
