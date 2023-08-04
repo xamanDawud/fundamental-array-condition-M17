5ceae078ed0761108725f84c78cdfaa2606e93d2
xamanDawud <ahmeddaudking100@gmail.com>
2023-08-04 12:47:21 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendsAge = [23, 53, 65, 23, 24, 53];
+var products = ["Light", "Fan", "Fridge", "Table"];
+var books = ["Linguistics", "Economics", "Toefl", "Philosophy"];
+
+console.log(books.length);
