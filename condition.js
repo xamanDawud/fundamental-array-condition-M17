ce3e14188ce0f2a8fb0338d0dfd1e617936c9e1d
xamanDawud <ahmeddaudking100@gmail.com>
2023-08-04 23:52:12 +0600

    basic conditon explored

---
+++ condition.js
@@ -0,0 +1,15 @@
+console.log(5 == 4);
+console.log(34 < 54);
+console.log(44 > 44);
+console.log(4 <= 5);
+console.log(5 >= 6);
+
+console.log(4 != 4);
+
+var result1 = 54;
+var result2 = 22;
+
+var height = 45;
+var height2 = 76;
+
+console.log(result1 < result2 && height < height2);
